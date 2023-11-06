70ce77378c3dbc82986f38acb9e94aa37b38793f
Federico C <fede03billy@gmail.com>
2023-11-06 21:53:09 +0100

    Added function generate_otp()

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import os
+import os, string, secrets
 from flask import Flask
 from postmarker.core import PostmarkClient
 
@@ -10,11 +10,21 @@ postmark = PostmarkClient(server_token=server_token)
 
 
 ### Functions
+## Generate OTP
+def generate_otp():
+    # Fetch the length from an environment variable; default to 6 if not set
+    length = int(os.getenv('CODE_LENGTH', 6))
+    length = max(length, 6)  # Ensure a minimum length of 6
+    length = min(length, 24) # Ensure a maximum length of 24
 
+    characters = string.digits
+
+    # Generate the OTP
+    return ''.join(secrets.choice(characters) for i in range(length))
 
 ### Flask App
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return generate_otp()
