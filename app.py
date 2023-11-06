0cf9e435f91ac0b93f7a0e23be0f0b8ca4aad093
Federico C <fede03billy@gmail.com>
2023-11-06 21:42:04 +0100

    Added postmark and dotenv

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask
+from postmarker.core import PostmarkClient
+
+load_dotenv()  # This will load the .env file variables into the environment
+server_token = os.getenv('POSTMARK_SERVER_TOKEN')
+
+postmark = PostmarkClient(server_token=server_token)
+
+
+### Functions
+
+
+### Flask App
 app = Flask(__name__)
 
 @app.route('/')
