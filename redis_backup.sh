07ee33707597ab5f8ef7964effc38ede14460255
Federico C <fede03billy@gmail.com>
2023-11-14 12:01:09 +0100

    Redis auto-backup added

--- redis_backup.sh
+++ redis_backup.sh
@@ -3,12 +3,13 @@
 
 while true; do
   redis-cli BGSAVE
+
+  # Capture the current date and time
+  current_time=$(date +"%Y-%m-%d %H:%M:%S")
+
+  # Log the time when the backup was saved
+  echo "Backup saved at $current_time"
+
   # Sleep for an hour
   sleep 3600
 done
-
-### Dockerfile spec
-# # Copy the backup script to the container
-# COPY redis_backup.sh /usr/local/bin/redis_backup.sh
-# # Set the backup script to be executable
-# RUN chmod +x /usr/local/bin/redis_backup.sh
